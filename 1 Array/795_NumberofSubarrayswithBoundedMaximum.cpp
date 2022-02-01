5f03a5d744c1de205493d8cd12239b1dde8de793
datshan-trivedi-10 <dbtrivedi03@gmail.com>
2022-02-01 16:35:02 +0530

    ARRAY 01/02/2022

---
+++ 1 Array/795_NumberofSubarrayswithBoundedMaximum.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/number-of-subarrays-with-bounded-maximum/
+#include <bits/stdc++.h>
+using namespace std;
+using lli = long long int;
+using ll = long long;
+#define endl '\n'
+
+class Solution
+{
+public:
+    int numSubarrayBoundedMax(vector<int> &nums, int left, int right)
+    {
+        // 2 ---> 8
+        // 2 9 2 5 6
+        // 2, 2 5 6
+        // 1, 2 , 2 5 , 256 5 56 6
+        // 3 4 / 2
+        
+        
+   
+
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    return 0;
+}
